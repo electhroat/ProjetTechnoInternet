3e507587c13a61d47d8cab2195a3861126ac4e47
Francis Lord <francislord95@hotmail.com>
2017-04-04 21:02:12 -0400

    WIP still working on it...

--- web-scrapper.js
+++ web-scrapper.js
@@ -1,5 +1,7 @@
-var Xray = require('x-ray');
-var x = Xray();
+var jsdom = require("jsdom");
+
+jsdom.defaultDocumentFeatures.FetchExternalResources = ["scripts"];
+jsdom.defaultDocumentFeatures.ProcessExternalResources = ["scripts"]; 
 
 exports.Scrape = function() {
 
@@ -8,40 +10,60 @@ exports.Scrape = function() {
     config.forEach(function (element) {
         console.log("config.forEach : " + element.url + " /// " + element.selector);
 
-        x(element.url, element.selector)(function(innerHtml){
-            console.log("X-ray : " + innerHtml);
-
-            events.push(element.readfunction(innerHtml));
+        jsdom.env({
+            url:element.url,
+            //scripts:["http://code.jquery.com/jquery.js"],
+            done:function(err, window) {
+                window.$(element.selector).each(function(index, foundNode) {
+                    events.push(element.readfunction(foundNode.innerHtml));
+                });
+            }
         });
     });
-
     return events;
-
 }
 
-var jsdom = require("jsdom");
+
 
 var config = [
     {
-        url:"http://ottawa.eventful.com/events/categories/music",
-        selector:'li [itemtype="http://schema.org/Event"]',
-        readfunction : function(innerHtml) {
-           console.log("config : " + innerHtml);
-           
-           var eventInfo= {};
+      url:"http://www.songkick.com/metro_areas/27381-canada-ottawa",
+      selector: null,
+      addFunction: function(err, window, events) {
+          window.$("li.with-date").each(function (index, element) {
+                var eventInfo = {};
+                
+                eventInfo.date = element.children("strong").children("time").text();
 
-           jsdom.env({
-               html:innerHtml,
-               scripts: ["http://code.jquery.com/jquery.js"], 
-               done: function(err, window) {
-                eventInfo.titre = window.$("h4").children("a").children("span").text();
-                eventInfo.lieu = window.$("div .event-meta").children("span").text();
-                eventInfo.date = window.$("div .event-meta").text();
-                eventInfo.image = window.$("a .tn-frame").children("img").attr("src");    
-            }});
-            return eventInfo;
-        }
-    }
+                var info = element.next();
+                eventInfo.titre = info.children("p .artists .summary").children("a").children("span").children("strong").text() + info.children("p").children("a").children("span").text();
+                eventInfo.lieu = info.children("p .location");
+
+          });
+      }
+
+    },
+    
+//    {
+//        url:"http://ottawa.eventful.com/events/categories/music",
+//        selector:'#events-list > li',
+//        readfunction : function(innerHtml) {
+//           console.log("config : " + innerHtml);
+//           
+//           var eventInfo= {};
+
+//           jsdom.env({
+//               html:innerHtml,
+//               scripts: ["http://code.jquery.com/jquery.js"], 
+//               done: function(err, window) {
+//                eventInfo.titre = window.$("h4").children("a").children("span").text();
+//                eventInfo.lieu = window.$("div .event-meta").children("span").text();
+//                eventInfo.date = window.$("div .event-meta").text();
+//                eventInfo.image = window.$("a .tn-frame").children("img").attr("src");    
+//            }});
+//            return eventInfo;
+//        }
+//    }
 ];
 
 /* **************************** */
